ea9a05f149e952eabfd7467f0326726f4e09183f
Tea Araki <teaaraki@hawaii.edu>
2025-04-25 18:50:37 -1000

    changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,52 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/ts/main.ts',
+  entry: './src/ts/main.ts', 
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    clean: true,
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/', 
   },
-  // Add other config like module rules and Svelte support here
+  module: {
+    rules: [
+    
+      {
+        test: /\.ts$/,
+        use: 'ts-loader', 
+        exclude: /node_modules/,
+      },
+     
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+     
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash].[ext]',
+              outputPath: 'assets/', 
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'], 
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/pages/index.html'
-    })
-  ]
+      template: './src/pages/index.html', 
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+  },
+  stats: {
+    children: true,
+  },
 };
