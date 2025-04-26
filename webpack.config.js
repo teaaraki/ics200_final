bf7e1f36378d1f76a4799f058cdaf2196e45d961
Tea Araki <teaaraki@hawaii.edu>
2025-04-25 19:50:56 -1000

    asia

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,58 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/ts/main.ts', 
+  entry: './src/ts/main.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: '/', 
+    clean: true, 
+    publicPath: './', 
   },
   module: {
     rules: [
-    
+      
       {
         test: /\.ts$/,
-        use: 'ts-loader', 
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
-     
+   
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
-     
+   
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[hash].[ext]',
-              outputPath: 'assets/', 
-            },
-          },
-        ],
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[hash][ext][query]',
+        },
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'], 
+    extensions: ['.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/pages/index.html', 
+      template: './src/pages/index.html',
+      filename: 'index.html',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/assets', to: 'assets' }, 
+      ],
     }),
   ],
   devServer: {
-    contentBase: './dist',
+    static: './dist',
+    open: true,
   },
   stats: {
     children: true,
   },
+  mode: 'development', 
 };
