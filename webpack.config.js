9a7abd1818e7d12b27d5ff13f485da7ab6febf97
Tea Araki <teaaraki@hawaii.edu>
2025-05-09 20:15:59 -1000

    europe + other updates

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,62 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: './src/ts/main.ts',
+  entry: './src/ts/main.ts', 
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true, 
+    clean: true,
     publicPath: './', 
   },
   module: {
     rules: [
-      
       {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
-   
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'], 
       },
-   
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'assets/[hash][ext][query]',
+          filename: 'assets/[hash][ext][query]', 
         },
       },
     ],
   },
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js'], 
   },
   plugins: [
+
     new HtmlWebpackPlugin({
       template: './src/pages/index.html',
       filename: 'index.html',
     }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/africa.html',
+      filename: 'africa.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/americas.html',
+      filename: 'americas.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/asia.html',
+      filename: 'asia.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/pages/europe.html',
+      filename: 'europe.html',
+    }),
     new CopyWebpackPlugin({
       patterns: [
-        { from: 'src/assets', to: 'assets' }, 
+        { from: 'src/assets', to: 'assets' },  
+        { from: 'src/styles', to: 'styles' },       
       ],
     }),
   ],
@@ -54,5 +69,5 @@ module.exports = {
   stats: {
     children: true,
   },
-  mode: 'development', 
+  mode: 'development',
 };
